7f8483b5de1319fad93011957de678981fddf67c
Giovanni Ferrara <giovanni.ferrara@agictech.com>
2017-09-27 19:23:33 +0200

    supporting inside iframe calls

--- gmap-locator-wrapper.js
+++ gmap-locator-wrapper.js
@@ -1,4 +1,19 @@
-/*
+/** 
+* @fileOverview 
+* @description
+* libreria per la ricerca e utilizzo della localizzazione su una mappa Google di un indirizzo.
+*
+* Utilizzata nelle pagine degli immobili per impostare la Lotitudine e Longitudine in base alla localizzazione sulla mappa.
+* [GmapLocatorWrapper]{@link module:GmapLocatorWrapper}
+*/
+/**
+ * @module GmapLocatorWrapper
+ * @description
+ * utilità per la localizzazione sulla mappa di un indirizzo
+ *
+ * Crea una finestra per la ricerca di un indirizzo e la geolocalizzazione dello stesso attraverso le Api Google Maps
+ * @requires lodash 3.10.0  
+ * @example
 <!DOCTYPE html>
 <html>
 
@@ -9,6 +24,30 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.0/lodash.js"></script>
     <script src="gmap-locator-wrapper.js"></script>
     <script>
+	  function updateSelectedLocation(e, marker) {
+            if (marker) {
+                $('#lat').val(marker.position.lat());
+                $('#lng').val(marker.position.lng());
+            }
+            var route = _.find(e.selected_address.address_components, function (address) {
+                return _.any(address.types, function (type) { return type == 'route' });
+            });
+            if (route) {
+                $('#address').val(route.long_name);
+            }
+            var number = _.find(e.selected_address.address_components, function (address) {
+                return _.any(address.types, function (type) { return type == 'street_number' });
+            });
+            if (number) {
+                $('#address-number'].HtmlID).val(number.long_name);
+            }
+            var postal_code = _.find(e.selected_address.address_components, function (address) {
+                return _.any(address.types, function (type) { return type == 'postal_code' });
+            });
+            if (postal_code) {
+                $('#zip-code').val(postal_code.long_name);
+            }
+        }
       function initializeLocator() {
         var options = {
           address: "", //start address search string
@@ -28,19 +67,15 @@
             div_wrapper: {"min-height": "500px", margin: 0, padding: 0}, 
             command_panel: {
               "position": "relative", 
-              //"top": "5px", 
-              //"left": "50%",
               "border-radius":"2px 0 0 2px",
               "box-sizing": "border-box",
               "-moz-box-sizing": "border-box",
               "outline": "none",
               "box-shadow": "0 2px 6px rgba(0, 0, 0, 0.3)",
-              
               "z-index": 5, 
               "background-color": "#fff", 
               "padding": "1px 1px 1px 10px", 
               "border": "1px solid #999", 
-              //"height": "32px", 
               "font-family": "'Roboto','sans-serif'", 
               "line-height": "30px"
             }, 
@@ -73,46 +108,16 @@
         google.maps.event.addListener(maplocator, 'markerplaced', function(e, marker) {
           console.log(e);
           console.log(marker);
-          
+          updateSelectedLocation(e, marker);
         });
         //  1.   'location_selected' when the user chooses the 'use selected location' button
         google.maps.event.addListener(maplocator, 'location_selected', function(e, marker) {
-               var route = _.find(e.selected_address.address_components, function(address){
-                  return _.any(address.types, function(type){return type == 'route'});
-                });
-                
-                var number  = _.find(e.selected_address.address_components, function(address){
-                  return _.any(address.types, function(type){return type == 'street_number'});
-                });
-                var postal_code = _.find(e.selected_address.address_components, function(address){
-                  return _.any(address.types, function(type){return type == 'postal_code'});
-                });         
-          alert(marker.position);
+          
+          updateSelectedLocation(e, marker);
           maplocator.unload();
         });
       }
       google.maps.event.addDomListener(window, 'load', initializeLocator);    
-	  
-	  
-		//alternatively...
-        
-//		var maplocator = new GMapLocatorWrapper("#panel-map-container", geoOptions);
-//        maplocator.load().then(function (locator, gmap) {
-//            google.maps.event.addListener(maplocator, 'location_selected', function (e, marker) {
-//                if (marker) {
-//                    $('#Latitude').val(marker.position.lat());
-
-//                    $('#Longitude').val(marker.position.lng());
-//                }
-
-//                //locator.unload();
-//
-//                
-//            });
-//
-//        });	  
-	  
-	  
     </script>
   </head>
 
@@ -121,12 +126,16 @@
     <div id="panel-map-container">
       
     </div>
+	<input type="text" id="lat" name="latitude" />
+	<input type="text" id="lng" name="longitude" />
+	<input type="text" id="address" name="address" />
+	<input type="text" id="address-number" name="address_number" />
+	<input type="text" id="zip-code" name="zip" />
   </body>
 
 </html>
 
- 
-
+  
 
 
 
@@ -134,24 +143,99 @@
 
 
 
-
+/** @class */
 var GMapLocatorWrapper = function(containerSelector, opts){
+	/** @var */
   this._id = _.uniqueId();
+	/** @var */
   this._div_wrapper = "map-wrapper-" + this._id;
+	/** @var */
   this._div_panel = "map-panel-" + this._id;
+	/** @var */
   this._text_address = "map-address-" + this._id;
+	/** @var */
   this._button_search = "map-search-" + this._id;
+	/** @var */
   this._select_button = "map-select-" + this._id;
+	/** @var */
   this._div_map = "map-canvas-" + this._id;
+	/** @var */
   this.geocoder = null;
+	/** @var */
   this.map = null;
+	/** @var */
   this.marker = null;
+	/** @var */
   this._geocoder = null;
+	/** @var */
   this.is_loaded = false;
+	/** @var */
   this.selected_address = null;
   if(!opts){
     opts = {};
   }
+/** 
+* @var 
+* @example
+{
+    readonly: false,
+    classes: {
+      div_wrapper: "", 
+      command_panel: "", 
+      address_text: "", 
+      address_button: "", 
+      select_button: "", 
+      map_div :""
+    },
+    script: {
+      params: {  
+        key: __PreamoGMapApiKey__,    
+        china: false,
+        v: '3.17',
+        libraries: '',
+        language: 'en',
+        sensor: 'false'
+      }
+    },
+    search_text: "Search",
+    select_text: "Use this location",
+    address: "",
+    latitude: null,
+    longitude: null,
+    styles: {
+      div_wrapper: {"min-height": "500px", margin: 0, padding: 0}, 
+      command_panel: {
+        "position": "relative", 
+        "border-radius":"2px 0 0 2px",
+        "box-sizing": "border-box",
+        "-moz-box-sizing": "border-box",
+        "outline": "none",
+        "box-shadow": "0 2px 6px rgba(0, 0, 0, 0.3)",
+        
+        "z-index": 5, 
+        "background-color": "#fff", 
+        "padding": "1px 1px 1px 10px", 
+        "border": "1px solid #999", 
+        "font-family": "'Roboto','sans-serif'", 
+        "line-height": "30px"
+      }, 
+      address_text: {"font-size": "15px",
+        "padding": "0 11px 0 13px",
+        "text-overflow": "ellipsis",
+        "margin-left": "12px",
+        "width": "40%"
+      }, 
+      address_button:{"font-size": "15px"}, 
+      select_button:{"font-size": "15px"}, 
+      map_div :""
+    },
+    map: {
+      zoom: 1,      
+      zoomControl: true,
+      panControl: true,
+    }
+*/
+
   var defaults = {
     readonly: false,
     classes: {
@@ -164,7 +248,7 @@ var GMapLocatorWrapper = function(containerSelector, opts){
     },
     script: {
       params: {  
-        key: "",    
+        key: __PreamoGMapApiKey__,    
         china: false,
         v: '3.17',
         libraries: '',
@@ -220,9 +304,10 @@ var GMapLocatorWrapper = function(containerSelector, opts){
   }*/
   this._options = _.defaultsDeep(opts, defaults);
   this._selector = containerSelector;
-  
+  this._window_context = self == top ? document : parent.document;
   
 }
+/** @returns {string} google maps api uri  */
 GMapLocatorWrapper.prototype.getScriptUrl = function() {
   if (this._options.script.params.china) {
     return 'http://maps.google.cn/maps/api/js?';
@@ -230,6 +315,7 @@ GMapLocatorWrapper.prototype.getScriptUrl = function() {
     return 'https://maps.googleapis.com/maps/api/js?';
   }
 }
+/** include lo script per google maps api  */
 GMapLocatorWrapper.prototype.includeScript = function() {
     var query, script, scriptId;
     scriptId = void 0;
@@ -246,9 +332,11 @@ GMapLocatorWrapper.prototype.includeScript = function() {
     script.src = this.getScriptUrl() + query;
     return document.body.appendChild(script);
 }
+/** @public  */
 GMapLocatorWrapper.prototype.isGoogleMapsLoaded = function() {
     return !(_.isUndefined(window.google) || _.isUndefined(window.google.maps));
 }
+/** @public  */
 GMapLocatorWrapper.prototype.loadGMap = function() {
   var deferred, randomizedFunctionName;
   deferred = $.Deferred();
@@ -273,15 +361,17 @@ GMapLocatorWrapper.prototype.loadGMap = function() {
   }
   return deferred.promise();
 }
+/** Inizializza  GMapLocatorWrapper: crea l'html, la mappa e inizializza gli eventi in ascolto */
+
 GMapLocatorWrapper.prototype.init = function(){
   this._geocoder = new google.maps.Geocoder();
   //var latlng = new google.maps.LatLng(lat, lng);
-  this.container = $(this._selector);
+  this.container = $(this._selector, this._window_context);  
   this._prepareHtml();
 
   //console.log(this.container.html());
 
-  this.map = new google.maps.Map(document.getElementById(this._div_map), this._options.map);
+  this.map = new google.maps.Map(this._window_context.getElementById(this._div_map), this._options.map);
   var self = this;
   google.maps.event.addListener(this.map, 'click', function(e) {
     self.reverseSearch(e.latLng);
@@ -304,23 +394,29 @@ GMapLocatorWrapper.prototype.init = function(){
   }
   this.is_loaded = true;
   //tilesloaded
-  $('#' + this._button_search).click(function(){
+  $('#' + this._button_search, this._window_context).click(function(){
     self.searchAddress();
   });
-  $('#' + this._text_address).keypress(function(evt){
+  $('#' + this._text_address, this._window_context).keypress(function(evt){
     if(evt.which == 13){
       self.searchAddress();
     }
   });
   if(this._options.readonly === true){
-    $('#' + this._select_button).attr("disabled", true);
+    $('#' + this._select_button, this._window_context).attr("disabled", true);
   }else{
-    $('#' + this._select_button).click(function(){
+    $('#' + this._select_button, this._window_context).click(function(){
       google.maps.event.trigger(self, 'location_selected', self, self.marker);   
     });
   }
   return self;
 }
+/** @public */
+GMapLocatorWrapper.prototype.getContext = function(){
+	return this._window_context;
+}
+
+/** @public */
 GMapLocatorWrapper.prototype.load = function(){
   var self = this;
   var deferred = $.Deferred();
@@ -330,12 +426,14 @@ GMapLocatorWrapper.prototype.load = function(){
   });
   return deferred.promise();
 }
+/** @public */
 GMapLocatorWrapper.prototype.unload = function(){
   this.geocoder = null;
   this.map = null;
   this.marker = null;
   this._detachHtml();
 }
+/** @protected */
 GMapLocatorWrapper.prototype._prepareHtml = function(){
   this._detachHtml();
   var element = $("<div id='" +this._div_wrapper +"'/>");
@@ -361,6 +459,7 @@ GMapLocatorWrapper.prototype._prepareHtml = function(){
   
   //console.log(this.container.html());
 }
+/** @protected */
 GMapLocatorWrapper.prototype._setElementStyle = function(el, opt_property){
   if(!_.isEmpty(this._options.classes[opt_property])){
     el.addClass(this._options.classes[opt_property]);
@@ -368,15 +467,18 @@ GMapLocatorWrapper.prototype._setElementStyle = function(el, opt_property){
     el.css(this._options.styles[opt_property]);
   }
 }
+/** @protected */
 GMapLocatorWrapper.prototype._detachHtml = function(){
   this.container.remove('#'+this._div_wrapper);
   
-  $('#'+this._div_wrapper).detach();
+  $('#'+this._div_wrapper, this._window_context).detach();
   this.is_loaded = false;
 }
+/** @public */
 GMapLocatorWrapper.prototype.isLoaded = function(){
   return this.is_loaded;
 }
+/** @public */
 GMapLocatorWrapper.prototype.reverseSearch = function(latLng){
   var self = this;
   self._geocoder.geocode({'location': latLng}, function(results, status) {
@@ -388,10 +490,11 @@ GMapLocatorWrapper.prototype.reverseSearch = function(latLng){
     }
   });     
 }
+/** @public */
 GMapLocatorWrapper.prototype.searchAddress = function(){
   var self = this;
   //console.log($('#' +this._text_address).val())
-  this._geocoder.geocode( { 'address': $('#' +this._text_address).val()}, function(results, status) {
+  this._geocoder.geocode( { 'address': $('#' +this._text_address, this._window_context).val()}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       self.map.setCenter(results[0].geometry.location);
       /*if(self.map.getZoom() < 18){
@@ -404,6 +507,7 @@ GMapLocatorWrapper.prototype.searchAddress = function(){
     }
   });    
 }
+/** @public */
 GMapLocatorWrapper.prototype.placeMarker = function(position, address) {
   if(this.marker){
     this.marker.setMap(null);
@@ -428,7 +532,7 @@ GMapLocatorWrapper.prototype.placeMarker = function(position, address) {
     infowindow.open(self.map, self.marker);
   });
   infowindow.open(self.map, self.marker);
-  $('#' +this._text_address).val(address.formatted_address);
+  $('#' +this._text_address, this._window_context).val(address.formatted_address);
   
   google.maps.event.trigger(this, 'markerplaced', this, this.marker);
 }
